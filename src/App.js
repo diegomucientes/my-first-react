d5e32a61dc7eba9396b1598a87811116bfc12386
diegomucientes <80188066+diegomucientes@users.noreply.github.com>
2021-04-11 21:00:13 -0400

    react-commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className ="try1">
+          HELLO DOJO!
+        </h1>
+        <h2>
+          Things i need to do:
+          <ul>
+            <li>Learn React</li> 
+            <li>Climb Mt Everest</li> 
+            <li>Run a Marathon</li> 
+            <li>Feed the Dogs</li> 
+          </ul>
+        </h2>
+
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
